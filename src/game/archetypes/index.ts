35f32a02f3652c65909922c6c591ab7ac3637d18
AnicetNgrt <anicet.nougaret@zohomail.eu>
2022-01-03 08:37:32 +0100

    refactored archetype displacement

--- src/game/archetypes/index.ts
+++ src/game/archetypes/index.ts
@@ -1,13 +1,7 @@
-import { Archetype, ArchetypeStack } from "../cards";
-import { ModifierMovePawn } from "../gameplay_modifiers";
-import type { PawnPlaced } from "../model";
-import { type SelectedTreeRoot, type SelectedTreeLeaf, select_one_currently_playable_pawn_if, OneTileSelector, filter_as_tiles, ChainedSelector } from "../selectors";
+import { ArchetypeDisplacement, ArchetypeStack } from "../cards";
 
-function mod(n, m) {
-    return ((n % m) + m) % m;
-}
 
-const SmallRivers = displacement_archetype(
+const SmallRivers = new ArchetypeDisplacement(
     "Small Rivers", 1, 
     [
         { x: 0, y: 1 },
@@ -24,7 +18,7 @@ export const StackSmallRivers = new ArchetypeStack([
     SmallRivers
 ], 0)
 
-const Knight = displacement_archetype(
+const Knight = new ArchetypeDisplacement(
     "Knight", 2, 
     [
         { x: 1, y: 2 },
@@ -42,51 +36,3 @@ export const StackKnight = new ArchetypeStack([
     Knight,
     Knight,
 ], 0)
-
-export function displacement_archetype(name: string, cost: number, pattern: { x: number, y: number; }[]): Archetype {
-    return {
-        name,
-        recyclable: true,
-        action_points_cost: cost,
-        modifier_factory: selected_tree => {
-            const selected_root = selected_tree as SelectedTreeRoot
-            const selected_pawn = (selected_root.children[0] as SelectedTreeLeaf).items[0].as_pawn()
-            const selected_tile = (selected_root.children[1] as SelectedTreeLeaf).items[0].as_tile()
-    
-            return new ModifierMovePawn(selected_pawn.id, {
-                x: selected_tile.x,
-                y: selected_tile.y
-            })
-        },
-        selector_factory: callback => {
-            let selected_pawn_id: number
-
-            const pawn_selector = select_one_currently_playable_pawn_if(
-                (session, pawn) => {
-                    return pawn.state == 'Placed' && pattern.some(shift => {
-                        const modifier = new ModifierMovePawn(pawn.id, {
-                            x: mod(pawn.x + shift.x, session.game.rules.width),
-                            y: mod(pawn.y + shift.y, session.game.rules.height)
-                        })
-                        return modifier.is_allowed(session.game) && modifier.is_playable(session.game, session.player)
-                    })
-                },
-                pawn => selected_pawn_id = pawn.id
-            )
-
-            const tile_selector = new OneTileSelector(
-                _ => {},
-                filter_as_tiles(
-                    (session, tile) => pattern.some(shift => {
-                        const pawn = session.game.pawns[selected_pawn_id] as PawnPlaced
-                        return mod(pawn.x + shift.x, session.game.rules.width) == tile.x
-                            && mod(pawn.y + shift.y, session.game.rules.height) == tile.y
-                            && tile.state == 'Empty'
-                    })
-                )
-            )
-            
-            return new ChainedSelector([pawn_selector, tile_selector], callback)
-        }
-    }
-}
--- src/game/cards.ts
+++ src/game/cards.ts
@@ -1,7 +1,7 @@
-import { ModifierConsumeCard } from "./gameplay_modifiers";
-import type { Game, Player } from "./model";
+import { ModifierMovePawn } from "./gameplay_modifiers";
+import type { PawnPlaced, Player } from "./model";
 import type { Modifier } from "./modifiers";
-import type { SelectedTree, Selector } from "./selectors";
+import { ChainedSelector, filter_as_tiles, OneTileSelector, type SelectedTree, type SelectedTreeLeaf, type SelectedTreeRoot, type Selector, select_one_currently_playable_pawn_if } from "./selectors";
 
 export class CardStack {
     constructor(
@@ -56,4 +56,60 @@ export class Archetype {
         public readonly modifier_factory: (selected_tree: SelectedTree) => Modifier,
         public readonly selector_factory: (callback: () => void) => Selector
     ) {}
+}
+
+export class ArchetypeDisplacement extends Archetype {
+    constructor(
+        public readonly name: string,
+        public readonly action_points_cost: number,
+        public readonly pattern: { x: number, y: number }[]
+    ) {
+        super(
+            name,
+            true,
+            action_points_cost,
+            selected_tree => {
+                const selected_root = selected_tree as SelectedTreeRoot
+                const selected_pawn = (selected_root.children[0] as SelectedTreeLeaf).items[0].as_pawn()
+                const selected_tile = (selected_root.children[1] as SelectedTreeLeaf).items[0].as_tile()
+        
+                return new ModifierMovePawn(selected_pawn.id, {
+                    x: selected_tile.x,
+                    y: selected_tile.y
+                })
+            },
+            callback => {
+                let selected_pawn_id: number
+                
+                const mod = (n, m) => ((n % m) + m) % m
+
+                const pawn_selector = select_one_currently_playable_pawn_if(
+                    (session, pawn) => {
+                        return pawn.state == 'Placed' && pattern.some(shift => {
+                            const modifier = new ModifierMovePawn(pawn.id, {
+                                x: mod(pawn.x + shift.x, session.game.rules.width),
+                                y: mod(pawn.y + shift.y, session.game.rules.height)
+                            })
+                            return modifier.is_allowed(session.game) && modifier.is_playable(session.game, session.player)
+                        })
+                    },
+                    pawn => selected_pawn_id = pawn.id
+                )
+    
+                const tile_selector = new OneTileSelector(
+                    _ => {},
+                    filter_as_tiles(
+                        (session, tile) => pattern.some(shift => {
+                            const pawn = session.game.pawns[selected_pawn_id] as PawnPlaced
+                            return mod(pawn.x + shift.x, session.game.rules.width) == tile.x
+                                && mod(pawn.y + shift.y, session.game.rules.height) == tile.y
+                                && tile.state == 'Empty'
+                        })
+                    )
+                )
+                
+                return new ChainedSelector([pawn_selector, tile_selector], callback)
+            }
+        )
+    }
 }
